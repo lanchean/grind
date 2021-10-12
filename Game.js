28c2158d7de4f55d742a1087bf10d449e656439f
Enckripted <71646814+Enckripted@users.noreply.github.com>
2021-10-12 12:38:50 -0500

    Begin on crafting

--- Game.js
+++ Game.js
@@ -1,1 +1,14 @@
+var Resources = {
+    "Wood": {
+        Amount = 0,
+        Recipe = {}
+    }
+};
 
+function Craft(ResourceName){
+    var Resource = Resources[ResourceName];
+
+    if(Resource.Recipe){
+        //TODO: Check if player can afford, else, draw it.
+    }
+}
