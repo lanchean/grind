d980815f55119e745bf823d338c9e259fb27a668
Enckripted <71646814+Enckripted@users.noreply.github.com>
2021-10-18 10:39:07 -0500

    Error logging

--- Game.js
+++ Game.js
@@ -1,6 +1,7 @@
 var Resources = {
     "Wood": {
         Amount: 0,
+        CraftingAmount = 1,
         Recipe: {}
     }
 };
@@ -11,8 +12,38 @@ function Craft(ResourceName){
     if(Resource.Recipe){
         var canAfford = true;
 
+        //Check if they can afford the item
         for(var i = 0; i < Resource.Recipe.length / 2; i++){
-            var 
+            var selectedResource = Resource.Recipe[i * 2];
+            var selectedAmount = Resource.Recipe[i * 2 + 1];
+
+            if(!Resource[selectedResource].Amount >= selectedAmount){
+                canAfford = false;
+                break;
+            }
+        }
+
+        if(canAfford){
+            //They can, give them the item
+            for(var i = 0; i < Resource.Recipe.length / 2; i++){
+                Resources[Resource.Recipe[i * 2]].Amount -= Resources[Resource.Recipe[i * 2 + 1]]
+            }
+
+            Resources[ResourceName].Amount += Resource.CraftingAmount
         }
     }
 }
+
+//Handles what the game should do when the web page loads
+function Init(){
+    window.onerror = function(error){
+        $('#errorlog').html('Error: ' + error);
+    }
+
+    //TODO: Gen all slots
+
+}
+
+Init();
+
+foo();
